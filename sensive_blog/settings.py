2f03526e9018143cf91ba3a16b78fb045cf3b32a
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-02-13 21:36:39 +0400

    fix mistakes

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -98,7 +98,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
-STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
+STATIC_ROOT = os.getenv("STATIC_ROOT")
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 
